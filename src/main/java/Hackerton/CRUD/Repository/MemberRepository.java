9d9994c744bd5360dcd6e389a041c9a2020a2a0a
kdhdd <kdso10@naver.com>
2024-07-27 19:33:30 +0900

    security

--- src/main/java/Hackerton/CRUD/Repository/MemberRepository.java
+++ src/main/java/Hackerton/CRUD/Repository/MemberRepository.java
@@ -3,5 +3,10 @@ package Hackerton.CRUD.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import Hackerton.CRUD.domain.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByLoginId(String loginId);
+    boolean existsByNickname(String nickname);
+    Optional<Member> findByLoginId(String loginId);
 }
---
+++ src/main/java/Hackerton/CRUD/dto/Request/JoinRequest.java
@@ -0,0 +1,33 @@
+package Hackerton.CRUD.dto.Request;
+
+import Hackerton.CRUD.UserRole;
+import Hackerton.CRUD.domain.Member;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class JoinRequest {
+
+    @NotBlank(message = "로그인 아이디가 비어있습니다.")
+    private String loginId;
+
+    @NotBlank(message = "비밀번호가 비어있습니다.")
+    private String password;
+    private String passwordCheck;
+
+    @NotBlank(message = "닉네임이 비어있습니다.")
+    private String nickname;
+
+    public Member toEntity(String encodedPassword) {
+        return Member.builder()
+                .loginId(this.loginId)
+                .password(encodedPassword)
+                .nickname(this.nickname)
+                .role(UserRole.USER)
+                .build();
+    }
+}
