3f4e136c17187973d8cf061d35e3e82aef056b1b
kdhdd <kdso10@naver.com>
2024-07-29 18:45:15 +0900

    swagger

--- src/main/java/Hackerton/CRUD/Config/SecurityConfig.java
+++ src/main/java/Hackerton/CRUD/Config/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
+                                .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**")
+                                .permitAll()
                                 .requestMatchers("/api/members/login").permitAll()
                                 .requestMatchers("/api/members/join").permitAll()
                                 .requestMatchers("/api/members/info").authenticated()
